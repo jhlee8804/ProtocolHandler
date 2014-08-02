433efbd0cddc90cce4ba943d32a591b244eb7846
jhlee8804 <jhlee.8804@gmail.com>
2014-08-03 02:26:54 +0900

    get key & value from query string

--- MyApp/main.cpp
+++ MyApp/main.cpp
@@ -4,11 +4,26 @@
 #include "stdafx.h"
 #include <iostream>
 #include <cstdio>
+#include <regex>
 
 
 int main(int argc, const char* argv[]) {
 
-  std::cout << "input: " << argv[argc - 1];
+  std::string input(argv[argc - 1]);
+  std::cout << "input: " << input << std::endl;
+
+  // get key&value from query string: http://stackoverflow.com/questions/2972186/cgicc-query-string-parsing
+  std::regex pattern("([\\w+%]+)=([^&]*)");
+  auto words_begin = std::sregex_iterator(input.begin(), input.end(), pattern);
+  auto words_end = std::sregex_iterator();
+
+  for (std::sregex_iterator i = words_begin; i != words_end; i++) {
+    std::string key = (*i)[1].str();
+    std::string value = (*i)[2].str();
+    
+    std::cout << key << " : " << value << std::endl;
+  }
+
   std::getchar();
 
   return 0;
